52ba70feb7de767e27d5635eb0b4300364f35a63
Gurveen <gurveensingh45@gmail.com>
2022-04-23 17:22:50 +0530

    Learnt Basics of React Native

--- App.js
+++ App.js
@@ -1,12 +1,55 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Image,
+  SafeAreaView,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  console.log("Hello World");
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* <Text numberOfLines={1} onPress={() => console.log("yo")}>
+        Hello World G
+      </Text>
+      <TouchableHighlight onPress={() => console.log("Tapped")}>
+        <Image
+          blurRadius={5}
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight> */}
+      {/* <Button
+        title="Click Me"
+        onPress={() =>
+          Alert.alert("Title", "Message", [
+            { text: "Yes", onPress: () => console.log("y") },
+            { text: "No", onPress: () => console.log("n") },
+          ])
+        }
+        color="green"
+      /> */}
+
+      <Button
+        title="Click Me 2"
+        onPress={() =>
+          Alert.prompt("Title", "Message", (text) => console.log(text))
+        }
+        color="green"
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +57,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    // alignItems: "center",
+    // justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
