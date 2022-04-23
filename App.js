77ce2e3e5073b511661602c589cebf177433cfbd
Gurveen <gurveensingh45@gmail.com>
2022-04-23 20:14:00 +0530

    Learnt Image, ImageBackgroynd, View, Style components

--- App.js
+++ App.js
@@ -1,64 +1,8 @@
-import {
-  StyleSheet,
-  Text,
-  Image,
-  SafeAreaView,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
-  Button,
-  Alert,
-  Platform,
-  StatusBar,
-} from "react-native";
+import React from "react";
+import { View } from "react-native";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  console.log("Hello World");
-
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <Text numberOfLines={1} onPress={() => console.log("yo")}>
-        Hello World G
-      </Text>
-      <TouchableHighlight onPress={() => console.log("Tapped")}>
-        <Image
-          blurRadius={5}
-          fadeDuration={1000}
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableHighlight> */}
-      {/* <Button
-        title="Click Me"
-        onPress={() =>
-          Alert.alert("Title", "Message", [
-            { text: "Yes", onPress: () => console.log("y") },
-            { text: "No", onPress: () => console.log("n") },
-          ])
-        }
-        color="green"
-      /> */}
-
-      <Button
-        title="Click Me 2"
-        onPress={() =>
-          Alert.prompt("Title", "Message", (text) => console.log(text))
-        }
-        color="green"
-      />
-    </SafeAreaView>
-  );
+  return <ViewImageScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+
+function WelcomeScreen(props) {
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/background.jpg")}
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.text}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "black",
+  },
+  loginButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#fc5c65",
+  },
+  registerButton: {
+    width: "100%",
+    height: 70,
+    backgroundColor: "#4ecdc4",
+  },
+});
+
+export default WelcomeScreen;
