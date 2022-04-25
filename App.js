51f23658030918bfe8927bc0f3b5c6372ba47866
Gurveen <gurveensingh45@gmail.com>
2022-04-25 13:14:13 +0530

    Implemented Image Picker

--- App.js
+++ App.js
@@ -1,6 +1,17 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useState } from "react";
+
+import Screen from "./app/components/Screen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState(null);
+
+  return (
+    <Screen>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
+    </Screen>
+  );
 }
--- app/config/styles.js
+++ app/config/styles.js
@@ -1,12 +1,27 @@
-import { Platform } from "react-native";
-
 import colors from "./colors";
 
 export default {
   colors,
   text: {
     color: colors.dark,
-    fontSize: 18,
-    fontFamily: Platform.OS === "android" ? "Roboto" : "Avenir",
+    ...Platform.select({
+      ios: {
+        fontSize: 18,
+        fontFamily: "Avenir",
+      },
+      android: {
+        fontSize: 16,
+        fontFamily: "Roboto",
+      },
+    }),
+  },
+  formField: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    borderColor: "#ddd",
+    borderRadius: 50,
+    borderWidth: 1,
+    marginVertical: 6,
+    overflow: "hidden",
   },
 };
